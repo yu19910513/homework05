b8de652c06460add80273c15c5c39a9943ebc5a7
Rex Yu <yu19910513@gmail.com>
2021-04-18 23:01:46 -0700

    final, not sure why gitignore does not ignore ;/

--- asset/JavaScripts/js.js
+++ asset/JavaScripts/js.js
@@ -1,3 +1,4 @@
+// for info storage and diplay
 var allText = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
 for (let j = 0; j < allText.length; j++) {
@@ -9,6 +10,8 @@ for (let j = 0; j < allText.length; j++) {
 for (let j = 0; j < allText.length; j++) {
     $("#textarea" + j).text(localStorage.getItem("message"+j));
 };
+
+// for timeline and color change
 
 setInterval(function timeClock() {
     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));

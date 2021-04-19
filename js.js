c51aadc0e1ba19c74e12a6b7114cce95adc9fa3a
Rex Yu <yu19910513@gmail.com>
2021-04-18 20:42:17 -0700

    color work

--- js.js
+++ js.js
@@ -1,4 +1,44 @@
+
 setInterval(function timeClock() {
-    $('#currentDay').text(moment())
-}, 1000);
-timeClock();
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    var allHours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+    for (let i = 0; i < allHours.length; i++) {
+        var jQHour = $('#'+i);
+
+        function present(){
+            jQHour.removeClass('future').addClass('present');
+        };
+
+        if (moment().hour() == i) {
+            present()
+        };
+
+    };
+
+    for (let i = 0; i < allHours.length; i++) {
+        var jQHour = $('#'+i);
+
+        function past(){
+            jQHour.removeClass('present').addClass('past');
+        };
+
+        if (moment().hour() > i) {
+            past()
+        };
+
+    };
+
+    for (let i = 0; i < allHours.length; i++) {
+        var jQHour = $('#'+i);
+
+        function future(){
+            jQHour.removeClass('past').addClass('future');
+        };
+
+        if (moment().hour() < i) {
+            future();
+        };
+
+    };
+
+} , 1000);
